081eaf10d9f5073dc805af2a83e573d6ecd49b31
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2026-07-23 22:27:39 +0200

    correct js path

--- modules/dashboard.nix
+++ modules/dashboard.nix
@@ -632,7 +632,7 @@ EOF
         <meta name="apple-mobile-web-app-title" content="🦆'Dash">
         <link rel="apple-touch-icon" href="/icon-192.png">
         <link rel="manifest" href="/site.webmanifest">
-        <script src="/script.js"></script>
+        <script src="./script.js"></script>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
